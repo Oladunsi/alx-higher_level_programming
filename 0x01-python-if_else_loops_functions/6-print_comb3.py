5946b999f89367ab83dcb1d85a3dbd657e434797
Oladunsi <okeoladunsi6@gmail.com>
2023-01-03 19:58:20 -0800

    added whitspace to make 6 print comb to pass code style check

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for units in range(0,10):
+for units in range(0, 10):
     for tens in range(units + 1, 10):
         if units == 8 and tens == 9:
-            print("{}{}".format(units,tens))
+            print("{}{}".format(units, tens))
         else:
-            print("{}{}".format(units,tens), end=", ")
+            print("{}{}".format(units, tens), end=", ")
