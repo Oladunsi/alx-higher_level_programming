6bed947b6c36e1dc6e14f657dcd8c01e88783c72
Oladunsi <okeoladunsi6@gmail.com>
2023-01-05 13:38:20 -0800

    added . and : to arguments print statments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
 
     args_count = len(sys.argv) - 1
     if args_count == 0:
-        print("0 arguments")
+        print("0 arguments.")
     elif args_count == 1:
-        print("1 argument")
+        print("1 argument:")
     else:
-        print("{} arguments".format(args_count))
+        print("{} arguments:".format(args_count))
     for ind in range(args_count):
         print("{}: {}".format(ind + 1, sys.argv[ind + 1]))
