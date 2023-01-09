e121b754cc42c57ed74b80e4e2f3d54793fb7906
Oladunsi <okeoladunsi6@gmail.com>
2023-01-09 08:14:18 -0800

    a function that finds the biggest integer of a list

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if len(sentence) == 0:
+    if sentence == "":
         return 0, None
     new = sentence.split(" ")
     return len(new[0]), new[0][0]
