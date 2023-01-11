b87437e56a5810a99db21b5a1d4e820698cb3b49
Oladunsi <okeoladunsi6@gmail.com>
2023-01-11 07:20:11 -0800

    a function that returns a list with all values multiplied by a number without using any loops

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,6 +3,6 @@
 def update_dictionary(a_dictionary, key, value):
     if a_dictionary.get(key, 0) == 0:
         a_dictionary[key] = value
-    else: 
+    else:
         a_dictionary[key] = value
     return a_dictionary
