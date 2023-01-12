481e40e137a14b0836a3dd3a2310d08ce726e0dd
Oladunsi <okeoladunsi6@gmail.com>
2023-01-11 18:43:12 -0800

    added a check to see if 10 best score funct is not an instance

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not isinstance(a_dictionary, dict) or a_dictionary is None:
         return None
     name = list(dict(sorted(a_dictionary.items(), key=lambda
                             item: item[1])).keys())[-1]
