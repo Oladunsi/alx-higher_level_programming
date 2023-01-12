8bf1d4b9ff8280713f67dba58d35d5737a984aff
Oladunsi <okeoladunsi6@gmail.com>
2023-01-11 18:48:45 -0800

    added a check to file 10 bestscore to see if dict is empty

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict) or a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary):
         return None
     name = list(dict(sorted(a_dictionary.items(), key=lambda
                             item: item[1])).keys())[-1]
