1bf57f00d3f74fea45bec0a6069727ef9d3ef9b1
Oladunsi <okeoladunsi6@gmail.com>
2023-01-11 18:53:06 -0800

    deleted crashed 10-bestscore file

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None or len(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     name = list(dict(sorted(a_dictionary.items(), key=lambda
                             item: item[1])).keys())[-1]
