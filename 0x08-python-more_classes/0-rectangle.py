7047ebc28a70c8b0d4e93475ade5b2d225f2af5d
Oladunsi <okeoladunsi6@gmail.com>
2023-01-30 20:53:43 -0800

    fix req

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,16 +1,15 @@
 #!/usrbin/python3
 # -*- coding: utf-8 -*-
 """
-    Created On 1/31/2023 4:28AM
-     @author: Oke Oladunsi Samuel
-    Defines a Rectangle class
+Created On 1/31/2023 4:28AM
+@author: Oke Oladunsi Samuel  
 """
 
 
 class Rectangle:
     """Represent a rectangle
 
-    Attributes:
-              empty
+    Attr:
+        empty
     """
     pass
