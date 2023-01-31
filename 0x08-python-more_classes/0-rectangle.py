5450256bb927601142e25761e7027b7c70471521
Oladunsi <okeoladunsi6@gmail.com>
2023-01-30 19:35:53 -0800

    Created empty rect class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usrbin/python3
+"""
+    CreatedOn: 1/31/2023 4:28AM
+    By: Oke Oladunsi Samuel
+    Defines a Rectangle class
+"""
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
