8a7f9ab87226205e54f8214f5b80c97c94c7c1af
Oladunsi <okeoladunsi6@gmail.com>
2023-02-08 20:59:52 -0800

    inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# 0-lookup.py
+# Oke Oladunsi Samuel
+"""Defines an object attribute lookup function."""
+
+
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/5-base_geomentry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# 5-base_geometry.py
+# Oke oladunsi Samuel
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
