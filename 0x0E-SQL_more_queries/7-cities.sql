94ea73ec29dcac92149e35d947278cc21c5240c6
Oladunsi <okeoladunsi6@gmail.com>
2023-03-17 09:25:34 -0700

    re-arange the cities columns

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,6 +5,6 @@ CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 -- creates table cities
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities`
     (`id` INT NOT NULL AUTO_INCREMENT UNIQUE,
-    `name` VARCHAR(256) NOT NULL,
-    `state_id` INT NOT NULL,
-    PRIMARY KEY(`id`), FOREIGN KEY(`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`));
+     `state_id` INT NOT NULL,
+     `name` VARCHAR(256) NOT NULL,
+     PRIMARY KEY(`id`), FOREIGN KEY(`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`));
