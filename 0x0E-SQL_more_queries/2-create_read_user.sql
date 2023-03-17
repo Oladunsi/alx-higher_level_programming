e08e650e976c40f514ca4ded353b05386bb5dbfa
Oladunsi <okeoladunsi6@gmail.com>
2023-03-17 08:24:06 -0700

    deleted the .swp file

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,5 +4,5 @@ CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'locahost'
     IDENTIFIED BY 'user_0d_2_pwd';
 -- Granting only SELECT to user_0d_2
-GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+GRANT USAGE ON * . * TO 'user_0d_2'@'localhost';
 GRANT SELECT ON `hbtn_0d_2` . * TO 'user_0d_2'@'localhost';
