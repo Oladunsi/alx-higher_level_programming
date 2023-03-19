217baade14f6a79057939d026076bb04f6f17df6
Oladunsi <okeoladunsi6@gmail.com>
2023-03-18 22:31:13 -0700

    13th task correction by calling the correct column and Table names

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- amount of shows associciated with each genre
-SELECT `tv_genre`.`name` AS `genre`,
-COUNT(`tv_show_genres`.`genre_id`) AS `number_of_shows`
-From `tv_genre` INNER JOIN `tv_show_genres`
-ON `tv_genre.id` = `tv_show_genres`.`genre_id`
+SELECT `tv_genres`.`name` AS `genre`,
+COUNT(*) AS `number_of_shows`
+From `tv_genres` INNER JOIN `tv_show_genres`
+ON `tv_genres`.`id` = `tv_show_genres`.`genre_id`
 GROUP BY `genre`
-ORDER BY `number_of_show` DESC;
+ORDER BY `number_of_shows` DESC;
