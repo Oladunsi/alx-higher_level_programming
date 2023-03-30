79a0337897080d42059f703efc0168f8767c594c
Oladunsi <okeoladunsi6@gmail.com>
2023-03-30 03:29:30 -0700

    error checking

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,9 +12,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_tree_height = 0, right_tree_height = 0;
 
-
 	if (tree == NULL)
 		return (0);
+
 	if (tree->left != NULL)
 		left_tree_height += 1 + binary_tree_height(tree->left);
 	if (tree->right != NULL)
