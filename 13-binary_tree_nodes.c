c5d668cd4209e80486f0fb297d73f1e2b3a35908
Oladunsi <okeoladunsi6@gmail.com>
2023-03-30 05:04:00 -0700

    updated the 13th task with the correct code

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,21 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - this measures the balance factor of a
- * binary tree.
+ * binary_tree_nodees - this counts the leaves in a binary tree
  * @tree: a pointer to the root node of the tree to transverse
  *
  * Return: 0 if tree is null or counts leaves in tree
  *
  */
-
-size_t binary_tree_balance(const binary_tree_t *tree)
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t tree_left = 0 tree_rigth = 0;
+	size_t tree_parent = 0;
 
 	if (tree == NULL)
 		return (0);
-	tree_left += binary_tree_height(tree->left);
-	tree_right += binary_tree_height(tree->right);
-	return (tree_left - tree_right);
+	if (tree->left != NULL || tree->right != NULL)
+		tree_parent += 1;
+	tree_parent += binary_tree_nodes(tree->left);
+	tree_parent += binary_tree_nodes(tree->right);
+	return (tree_parent);
 }
