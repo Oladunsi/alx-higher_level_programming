6d024b2e9e3619592852370f822f4670ac7533e8
Oladunsi <okeoladunsi6@gmail.com>
2023-03-30 03:25:25 -0700

    error confirmation on the 9th requirements

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,12 +4,14 @@
  * binary_tree_height - this gets tree height
  * @tree: a pointer to the root node of the tree to transverse
  *
- * Returns: 0 if tree is null or size of tree
+ * Returns: 0 if size_t tree is null or size of tree
  *
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_tree_height = 0, right_tree_height = 0;
+
 
 	if (tree == NULL)
 		return (0);
