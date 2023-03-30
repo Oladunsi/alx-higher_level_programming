3a87619827d037225c92842c00ba29b09d00de72
Oladunsi <okeoladunsi6@gmail.com>
2023-03-30 09:12:40 -0700

    updated 14th task with better code structure

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -34,11 +34,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL)
-		left_tree_height += 1 + binary_tree_height(tree->left);
-	if (tree->right != NULL)
-		right_tree_height += 1 + binary_tree_height(tree->right);
-
+	left_tree_height += 1 + binary_tree_height(tree->left);
+	right_tree_height += 1 + binary_tree_height(tree->right);
 	if (left_tree_height > right_tree_height)
 		return (left_tree_height);
 	return (right_tree_height);
