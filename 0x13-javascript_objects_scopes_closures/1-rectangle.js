d869606ba305ece47e379a628d911bbe99a88ad1
Oladunsi <okeoladunsi6@gmail.com>
2023-04-12 20:58:29 -0700

    created an rectangle where If w or h is equal to 0 or not a positive integer, create an empty object

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+// creating a rectangle class
+
+module.exports = class Rectangle { constructor(w, h) {
+                this.width = w;
+                this.height = h;
+   }
+};
---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+// creating a rectangle class
+
+module.exports = class Recatangle {
+	constructor (w, h) {
+		if (w > 0 && h > 0) { [this.width, this.height] = [w, h] };
+	}
+};
