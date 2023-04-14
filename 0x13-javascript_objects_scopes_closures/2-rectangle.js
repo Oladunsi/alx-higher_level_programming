f694d132dfe3e01634639e775f309ab9ff6df466
Oladunsi <okeoladunsi6@gmail.com>
2023-04-13 19:51:54 -0700

    corrected the typo errors in class name for 2&3 rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 // creating a rectangle class
 
-module.exports = class Recatangle {
+class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+    if (w > 0 && h > 0) {
+	    [this.width, this.height] = [w, h];
+    }
   }
-};
+}
+module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,12 +1,17 @@
 #!/usr/bin/node
 // creating a rectangle class
 
-module.exports = class Recatangle {
+class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+    if (w > 0 && h > 0) {
+	    [this.width, this.height] = [w, h];
+    }
   }
 
   print () {
-    for (let i = 0; i < this.height; i++) { console.log('X'.repeat(this.width)); }
+    for (let i = 0; i < this.height; i++) {
+	    console.log('X'.repeat(this.width));
+    }
   }
-};
+}
+module.exports = Rectangle;
