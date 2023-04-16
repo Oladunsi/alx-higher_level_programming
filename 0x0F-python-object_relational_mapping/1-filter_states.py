71fcd51509cd728da8c8904abca7f779efe6a5a1
Oladunsi <okeoladunsi6@gmail.com>
2023-04-16 16:17:59 -0700

    added fixes to 1 filter file

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -21,8 +21,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=username,
                          password=password, db=database_name, port=3306)
     cur = db.cursor()
-    num_rows = cur.execute('SELECT * FROM states \
-                           WHERE states.name like "N%" ORDER BY states.id;')
+    num_rows = cur.execute('SELECT * FROM `states` \
+                           WHERE `name` like "N%" ORDER BY `id`;')
     rows = cur.fetchall()
     for row in rows:
         print(row)
