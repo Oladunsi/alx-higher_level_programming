c1eea4b507762326bbdc3cdb53e3de8f63d8e32d
Oladunsi <okeoladunsi6@gmail.com>
2023-04-16 05:59:45 -0700

    a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""@author: Oke Oladunsi Samuel
+   dated: Sun 4 Aug 13:21:33 2023
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) != 4:
+        print("Entry Not Correct!!!\n \
+                Usage:{}  Username Password Database_name".format(args[0]))
+        exit(1)
+
+    username = args[1]
+    password = args[2]
+    database_name = args[3]
+    db = MySQLdb.connect(host='localhost', user=username,
+                         password=password, db=database_name, port=3306)
+    cur = db.cursor()
+    num_rows = cur.execute(
+                    'SELECT * FROM states WHERE states.name like \'N%\';')
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close
+    db.close
