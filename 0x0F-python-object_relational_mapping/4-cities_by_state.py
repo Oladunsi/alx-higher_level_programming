7bb0ef08059137cbf14d38339a8b8780e010ba0f
Oladunsi <okeoladunsi6@gmail.com>
2023-04-16 17:35:00 -0700

    a script that lists all cities from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""@author: Oke Oladunsi Samuel
+   dated: Sun 4 Aug 13:21:33 2023
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) != 4:
+        print("Entry Not Correct!!!\n \
+                Usage:{}  Username Password Database_name".format(args[0]))
+        exit(1)
+
+    username = args[1]
+    password = args[2]
+    database_name = args[3]
+    state_name = args[4]
+    db = MySQLdb.connect(host='localhost', user=username,
+                         password=password, db=database_name, port=3306)
+    cur = db.cursor()
+    num_rows = cur.execute("SELECT * FROM `id`, `name`, `state`.`name` \
+                           FROM `cities` INNER JOIN `states` \
+                           `cities`.`id` = `states`.`id` ORDER BY `cities`.`id`;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
