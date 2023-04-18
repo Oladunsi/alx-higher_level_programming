f9811a6c6d981ac1a9b42c82449d4194ca02a60e
Oladunsi <okeoladunsi6@gmail.com>
2023-04-18 12:07:35 -0700

    requirement correction

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -18,5 +18,7 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state, city in session.query(State, City).filter(State.id == City.state_id).order_by(City.id):
-       print("{}: ({}) {}".format(state.name, city.id, city.name))
+    for state, city in session.query(State, City)\
+                              .filter(State.id == City.state_id)\
+                              .order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
