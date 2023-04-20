2e752dd5fb6e7cae4d49db31c2e9542fd62cac68
Oladunsi <okeoladunsi6@gmail.com>
2023-04-20 08:49:25 -0700

    Improve the files model_city.py and model_state.py, and save them as relationship_city.py and relationship_state.py

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""lists all states in the db hbtn_0e_6_usa
+   Usage: ./8-model_state_fetch_first.py <username> \
+           <password> <username>
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_city import City 
+from relationship_state import State
+
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.add(City(name="San Francisco", state=State(name="Califionia")))
+    session.commit()
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This is defines a State model that inherits form
+SQLAlchemy base class and links db Tables using MySQLdb
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from relationship_city import Base, City
+
+
+class State(Base):
+    """Representation of state class in MySQL db
+
+    __tablename__: a representation of state TABLE in db
+                  that stores State instances
+    id (sqlalchemy.Integer): instance of state.id
+    name (sqlalchemy.String): instance of state.name
+    """
+
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", backref="state", cascade="all, delete")
