a679ba0b39ade939d4a18a217af9a01b6d310769
Oladunsi <okeoladunsi6@gmail.com>
2023-05-25 16:16:26 +0000

    parse input as int

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const movieId = process.argv[2];
+const movieId = parseInt(process.argv[2]);
 const url  = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 let data;
 request(url, function (error, response, body) {
