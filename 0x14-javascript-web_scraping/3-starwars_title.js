f6e0e28bda95190cbd8e124b233bc17f99da1364
Oladunsi <okeoladunsi6@gmail.com>
2023-05-25 16:09:36 +0000

    spacing error fixes

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,9 +4,9 @@ const movieId = process.argv[2];
 const url  = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 request(url, function (error, response, body) {
   if (error) {
-  console.log(error);
+    console.log(error);
   } else {
-  data = JSON.parse(body);
-  console.log(data['title']);
+    data = JSON.parse(body);
+    console.log(data['title']);
   }
 });
