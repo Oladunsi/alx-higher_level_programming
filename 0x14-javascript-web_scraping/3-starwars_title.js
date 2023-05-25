52270f25324a80416c82d755abba9fe7aa88c7b5
Oladunsi <okeoladunsi6@gmail.com>
2023-05-25 16:18:52 +0000

    requirement fixes

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,11 +3,12 @@ const request = require('request');
 const movieId = parseInt(process.argv[2]);
 const url  = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 let data;
+
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     data = JSON.parse(body);
-    console.log(data['title']);
+    console.log(data.title);
   }
 });
